700781f419f69b165cbd5c6c156cdf4ac94893dc
abenito <abenito.org@gmail.com>
2025-08-01 17:47:34 +0100

    visualisation

---
+++ visualisation.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+import requests
+import tqdm
+import numpy as np
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+
+plt.title("Nominal GDP")
+plt.ylabel("$ billions")
+
+# movies
+movies = ["Anniel Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
+
+plt.bar(range(len(movies)), num_oscars)
