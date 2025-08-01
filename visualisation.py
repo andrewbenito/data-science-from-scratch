8941d4996e0981c5106acb93143b98d19b743710
abenito <abenito.org@gmail.com>
2025-08-01 18:14:49 +0100

    fork from joel grus book "Data Science from Scratch"

--- visualisation.py
+++ visualisation.py
@@ -7,12 +7,78 @@ years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
 
 plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
-
 plt.title("Nominal GDP")
 plt.ylabel("$ billions")
+
+plt.savefig('im/viz_gdp.png')
+plt.clf()
 
 # movies
 movies = ["Anniel Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
 num_oscars = [5, 11, 3, 8, 10]
 
 plt.bar(range(len(movies)), num_oscars)
+plt.title("Oscar-winning movies")
+plt.ylabel("Number of Oscars")
+
+plt.savefig('im/viz_movies.png')
+plt.clf()
+
+from collections import Counter
+grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
+
+# Bucket grades by decile, but put 100 in with the 90s
+histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+
+plt.bar([x + 5 for x in histogram.keys()],  # Shift bars right by 5
+        histogram.values(),                 # Give each bar its correct height
+        10,                                 # Give each bar a width of 8
+        edgecolor=(0, 0, 0))                # Black edges for each bar
+
+plt.axis([-5, 105, 0, 5])                  # x-axis from -5 to 105,
+                                           # y-axis from 0 to 5
+
+plt.xticks([10 * i for i in range(11)])    # x-axis labels at 0, 10, ..., 100
+plt.xlabel("Decile")
+plt.ylabel("# of Students")
+plt.title("Distribution of Exam 1 Grades")
+# plt.show()
+
+plt.savefig('im/viz_grades.png')
+plt.clf()
+
+# Example 3
+friends = [ 70,  65,  72,  63,  71,  64,  60,  64,  67]
+minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+labels =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+plt.scatter(friends, minutes)
+
+# label each point
+for label, friend_count, minute_count in zip(labels, friends, minutes):
+    plt.annotate(label,
+        xy=(friend_count, minute_count), # Put the label with its point
+        xytext=(5, -5),                  # but slightly offset
+        textcoords='offset points')
+
+plt.title("Daily Minutes vs. Number of Friends")
+plt.xlabel("# of friends")
+plt.ylabel("daily minutes spent on the site")
+# plt.show()
+
+
+plt.savefig('im/viz_scatterplot.png')
+plt.gca().clear()
+
+test_1_grades = [ 99, 90, 85, 97, 80]
+test_2_grades = [100, 85, 60, 90, 70]
+
+plt.scatter(test_1_grades, test_2_grades)
+plt.title("Axes Aren't Comparable")
+plt.xlabel("test 1 grade")
+plt.ylabel("test 2 grade")
+# plt.show()
+
+
+plt.savefig('im/viz_scatterplot_axes_not_comparable.png')
+plt.clf()
